4d87b9389b15943d58188ccacf4a08bc53721767
VinogradovDanya <vinogradov.do@phystech.edu>
2021-12-06 11:41:51 +0300

    almost final version

--- Matplotlib/Episode _3/main_3.py
+++ Matplotlib/Episode _3/main_3.py
@@ -1,42 +1,39 @@
 import matplotlib.pyplot as plt
-from collections import defaultdict
+import collections
 
-dct1 = defaultdict(list)
-dct2 = defaultdict(list)
+dct1 = collections.defaultdict(list)
+dct2 = collections.defaultdict(list)
 
 with open("students.csv") as file:
     line = file.readlines()
     for i in line:
         x = int(i.split(';')[2])
-        dct1[i.split(';')[0]].append(x)
-        dct2[i.split(';')[1]].append(x)
+        dct1[i.split(';')[0]].append(int(x))
+        dct2[i.split(';')[1]].append(int(x))
 
 fig, ax1 = plt.subplots(2, 1, figsize=[12, 12])
 w = 0.6
 
-dct1_new = defaultdict(list)
-dct2_new = defaultdict(list)
-
 for key in dct1:
+    a = [0] * 11
     for i in range(11):
-        dct1[key].count(i)
-        dct1_new[key].append([i, dct1[key].count(i)])
+        a[i] = dct1[key].count(i)
+    dct1[key] = a
 
 for key in dct2:
+    a = [0] * 11
     for i in range(11):
-        dct2[key].count(i)
-        dct2_new[key].append([i, dct2[key].count(i)])
+        a[i] = dct2[key].count(i)
+    dct2[key] = a
 
-print(dct1_new)
-print(dct2_new)
 
 for i in range(1, 11):
-    rez1 = [dct1_new[key][i][1] for key in dct1_new]
-    oldst = [sum(dct1_new[key][j] for j in range(i)) for key in dct1_new]
-    rez2 = [dct2_new[key][i][1] for key in dct2_new]
-    olddt = [sum(dct2_new[key][j] for j in range(i)) for key in dct2_new]
-    ax1[0].bar(list(dct1_new.keys()), rez1, w, bottom=oldst, label='%d' % i)
-    ax1[1].bar(list(dct2_new.keys()), rez2, w, bottom=olddt, label='%d' % i)
+    rez1 = [dct1[key][i] for key in dct1]
+    oldst = [sum(dct1[key][j] for j in range(i)) for key in dct1]
+    rez2 = [dct2[key][i] for key in dct2]
+    olddt = [sum(dct2[key][j] for j in range(i)) for key in dct2]
+    ax1[0].bar(list(dct1.keys()), rez1, w, bottom=oldst, label='%d' % i)
+    ax1[1].bar(list(dct2.keys()), rez2, w, bottom=olddt, label='%d' % i)
 
 
 ax1[0].set_title('Marks per prep')
